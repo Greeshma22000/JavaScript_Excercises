e93ffc6003113c41ec2ac0907143d4186f08925a
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-11-21 15:47:16 +0530

    .

---
+++ get_set_classes.js
@@ -0,0 +1,18 @@
+class Thermostat{
+    constructor(temp){
+        this._temp = temp;
+    }
+    get temperature() {
+        return 5/9 * (this._temp - 32);
+    }
+    set temperature(updatedTemp){
+        this._temp = updatedTemp * 9.0 / 5 + 32;
+    }
+}
+
+const thermos = new Thermostat(76);
+
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature;
+console.log(temp);
